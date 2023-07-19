cb0386988791a06aa53658fb1f6389f575f8c58e
Techelijah <spyceelijah@gmail.com>
2023-07-19 09:47:38 -0700

    0x0D. C - Preprocessor

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,15 @@
+// defines a macro named SIZE as an abbreviation for the token 1024.
+
+
+#include <stdio.h>
+
+#define SIZE 1024
+
+int main() {
+			
+	printf("size is : %d\n", SIZE);
+
+
+return 0;
+
+}
