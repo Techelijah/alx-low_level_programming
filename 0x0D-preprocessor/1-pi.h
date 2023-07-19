ed8a4d8be426efdf6597a421d5806c166d2b657a
Techelijah <spyceelijah@gmail.com>
2023-07-19 10:45:30 -0700

    0x0D. C - Preprocessor

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,16 @@
+// defines a macro named pi as an abbreviation for the token 3.14159265359.
+
+
+#include <stdio.h>
+
+#define pi 3.14159265359
+
+int main()
+{
+			
+	printf("size is : %f\n", pi);
+
+
+	return 0;
+
+}
